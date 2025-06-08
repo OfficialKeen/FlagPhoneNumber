ae59f2b014f062c4340d05c3e482ba0cd8230a65
Keen <zhekeen90@gmail.com>
2025-06-08 11:16:17 +0700

    Package

--- Sources/Helpers/Bundle+Extension.swift
+++ Sources/Helpers/Bundle+Extension.swift
@@ -8,12 +8,15 @@
 import Foundation
 
 public extension Bundle {
-    @objc static var FlagIcons: Bundle = {
+    @objc static let FlagIcons = Bundle.FlagPhoneNumber()
+
+    @objc static func FlagPhoneNumber() -> Bundle {
         let bundle = Bundle(for: FPNTextField.self)
+        
         if let path = bundle.path(forResource: "FlagPhoneNumber", ofType: "bundle") {
             return Bundle(path: path)!
         } else {
             return bundle
         }
-    }()
+    }
 }
